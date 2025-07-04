58e1c9ea61cacf51a539b01a76f2912698061dce
mehrab_mahi <mehrab.hosen@streamstech.com>
2025-07-05 05:26:40 +0600

    new migration added

---
+++ GoBangladesh.Infra.Data/Migrations/20250704232431_TripModifiedAgain.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoBangladesh.Infra.Data.Migrations
+{
+    public partial class TripModifiedAgain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "Trips",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_SessionId",
+                table: "Trips",
+                column: "SessionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trips_Sessions_SessionId",
+                table: "Trips",
+                column: "SessionId",
+                principalTable: "Sessions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trips_Sessions_SessionId",
+                table: "Trips");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Trips_SessionId",
+                table: "Trips");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "Trips",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
